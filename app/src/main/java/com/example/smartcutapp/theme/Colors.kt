f1941ef5437a96b054bf7d831c83f1aa026e1b5f
YoruKodama <sr50047866@gmail.com>
2026-01-31 19:39:34 +0300

    Update AppTheme,create MainScreen

---
+++ app/src/main/java/com/example/smartcutapp/theme/Colors.kt
@@ -0,0 +1,23 @@
+package com.example.smartcutapp.theme
+
+import androidx.compose.ui.graphics.Color
+
+//Темная тема
+
+val DarkBackground = Color(0xFF1A1A1A)
+val DarkCard = Color(0xFF2A1810)
+val DarkTextPrimary = Color(0xFFFF8C42)
+val DarkTextSecondary = Color(0xB3FF8C42)
+val DarkDisconnected = Color(0xFF757575)
+//Светлая тема
+
+val LightBackground = Color(0xFFFAFAFA)
+val LightCard = Color(0xFFFFFFFF)
+val LightTextPrimary = Color(0xFF212121)
+val LightTextSecondary = Color(0xFF757575)
+val LightDisconnected = Color(0xFFBDBDBD)
+val Orange = Color(0xFFFF8C42)
+val LightOrange = Color(0xFFFF9B6B)
+val Green = Color(0xFF4CAF50)
+val White = Color(0xFFFFFFFF)
+val OrangeAlpha = Color(0x33FF8C42)
